22a7834a7b8a6b4472d586c59caa2895251850ac
hariskhan802 <96125873+hariskhan802@users.noreply.github.com>
2025-11-18 09:55:35 +0000

    feat: uncomment styles import in layout.js and add ResumeViewer component in page.js

--- openai-resume-generator/src/app/page.js
+++ openai-resume-generator/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import ResumeForm from "@/components/ResumeForm";
+import ResumeViewer from "@/components/ResumeViewer";
 
 export default function Home() {
   return (
     <div >
         <ResumeForm />
+        <ResumeViewer />
     </div>
   );
 }
